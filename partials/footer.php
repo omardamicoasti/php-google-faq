1edb3c9a4e150ffeafd87a55bf1353e89295b76f
Omar D'Amico <omar.damico.asti@gmail.com>
2021-04-27 14:32:30 +0200

    Secondo push

---
+++ partials/footer.php
@@ -0,0 +1,18 @@
+<footer>
+    <div class="container">
+        <ul>
+            <li>Google</li>
+            <li>Tutto su Google</li>
+            <li>Privacy</li>
+            <li>Termini</li>
+        </ul>
+        <select name="language">
+            <option value="italiano">Italiano</option>
+            <option value="Inglese">Inglese</option>
+            <option value="Francese">Francese</option>
+            <option value="Tedesco">Tedesco</option>
+            <option value="Spagnolo">Spagnolo</option>
+            <option value="Cinese">Cinese</option>
+        </select>
+    </div>
+</footer>
---
+++ partials/header.php
@@ -0,0 +1,15 @@
+<header>
+    <div id="upperHeader">
+        <img src="img/google_logo.png" alt="Google logo">
+        <h1>Privacy e termini</h1>
+    </div>
+    <div id="lowerHeader">
+        <ul>
+            <li>Introduzione</li>
+            <li>Norme sulla privacy</li>
+            <li>Termini di servizio</li>
+            <li>Tecnologie</li>
+            <li>Domande frequenti</li>
+        </ul>
+    </div>
+</header>
